a39e3b43c667a0c96b784d9e5a2f9d93f0e22fa9
zhangxin <1271877638@qq.com>
2022-09-06 18:12:40 +0800

    feat:登入页完成布局

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    黑马头条
-    <span class="toutiao toutiao-gengduo"></span>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button> 6666
-    <div class="div">登入</div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -16,9 +9,5 @@ export default {}
 </script>
 
 <style scoped>
-.div{
-  width: 700px;
-  height: 100px;
-  background-color: green;
-}
+
 </style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <van-nav-bar title="登入" class="nav-bar" />
+    <van-form @submit="onSubmit" class="from">
+      <van-field
+        v-model="username"
+        name="mobile"
+        placeholder="请输入手机号"
+        :rules="[{ required: true, message: '请填写用户名' },
+        {pattern:/^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
+        message:'手机格式错误'
+        }
+
+        ]"
+      >
+      <template #label>
+        <span class="toutiao toutiao-shouji"></span>
+      </template>
+      </van-field>
+      <van-field
+        v-model="code"
+        name="code"
+        placeholder="请输入验证码"
+        :rules="[{ required: true, message: '请填写密码' },
+        {pattern:/^\d{6}$/,
+        message:'验证码格式错误'
+        }]"
+      >
+        <template #label>
+        <span class="toutiao toutiao-yanzhengma"></span>
+      </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button  block type="info" native-type="submit"
+          >提交</van-button
+        >
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobile: '',
+      code: ''
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.nav-bar {
+  background-color: yellowgreen;
+  :deep(.van-nav-bar__title) {
+    color: #fff;
+  }
+}
+:deep(.from){
+  .van-cell__title{
+    flex: 1;
+  }
+  .van-cell__value {
+    flex: 20;
+  }
+  .toutiao {
+    font-size: 40px;
+  }
+}
+</style>
