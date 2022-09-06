4241a33fa2b842dc70c88b6da0dfc0b9f0c9803e
zhangxin <1271877638@qq.com>
2022-09-06 16:13:09 +0800

    feat:postcss配置完成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>黑马头条  6666</div>
+  <div>
+    黑马头条
+    <span class="toutiao toutiao-gengduo"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button> 6666
+    <div class="div">登入</div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.div{
+  width: 700px;
+  height: 100px;
+  background-color: green;
+}
+</style>
